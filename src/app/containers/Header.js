f30184488edc0a9edae231d514e0eb54e9b58048
Gerar <gerarsuaverdez@gmail.com>
2025-02-15 14:46:04 -0800

    added user log in with Firebase Authentification

---
+++ src/app/containers/Header.js
@@ -0,0 +1,26 @@
+"use client"
+import { signInWithGoogle, signOutFromGoogle } from "@/lib/firebase";
+import { useAuth } from "@/lib/useAuth";
+import Link from 'next/link';
+
+export const Header = () => {
+    const { user, loading } = useAuth();
+
+    console.log(user, loading);
+
+    return (
+        <div className="py-4 px-8 border-b font-medium flex justify-between">
+            <Link href = {'/'}>Rmd</Link>
+
+            {!loading && (
+                <button 
+                type="button" 
+                className="hover:underline" 
+                onClick={ user ? signOutFromGoogle : signInWithGoogle}
+                >
+                    Log { user ? <>Out</>:<>In</> }
+                </button>
+            )}
+        </div>
+    );
+};
---
+++ src/lib/firebase.js
@@ -0,0 +1,42 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDAHxe5f3gz74qP59VHvRyx7uQ0h9EcfP8",
+  authDomain: "rmd-2025-gerar.firebaseapp.com",
+  projectId: "rmd-2025-gerar",
+  storageBucket: "rmd-2025-gerar.firebasestorage.app",
+  messagingSenderId: "121310753546",
+  appId: "1:121310753546:web:b3e2f4d71abd06148140f3"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+export const signInWithGoogle = async () => {
+  try {
+    const result = await signInWithPopup(auth, new GoogleAuthProvider());
+    const user = result.user;
+
+    window.alert(`Signed in with ${user.email}`);
+  } catch (e) {
+    window.alert(e.message);
+  }
+};
+
+export const signOutFromGoogle = async () => {
+  try {
+    await signOut(auth);
+
+    window.alert('Signed out!');
+  } catch (e) {
+    window.alert(e.message);
+  }
+};
