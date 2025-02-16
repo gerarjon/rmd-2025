97671ceb920d80123740840979725202efda3d15
Gerar <gerarsuaverdez@gmail.com>
2025-02-15 16:28:44 -0800

    created Schools Data for firestore

---
+++ src/lib/reads.js
@@ -0,0 +1,11 @@
+import { collection, getDocs, query } from "firebase/firestore";
+import { db } from "./firebase";
+
+export const getSchools = async () => {
+    return (await getDocs(query(collection(db, "schools")))).docs.map(
+        (docSnap) => ({
+            ...docSnap.data(),
+            id: docSnap.id,
+        })
+    );
+};
